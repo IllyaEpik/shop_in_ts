b6c7a45e38ed63eef50c361d58875cf6e5511cf7
Renat19Belei <resyaresya1908@gmail.com>
2025-12-18 21:02:36 +0200

    user

--- src/User/UserRepository.ts
+++ src/User/UserRepository.ts
@@ -1,23 +1,30 @@
 import prisma from "../db/prismaDB";
-// import Prisma from "../db/prismaDB";
 import { IRepositoryContract } from "./UserTypes";
 
-export const repositoryMethods:IRepositoryContract = {
-    getUserByEmail: async (email) => {
-        const user = await prisma.user.findUnique({
-            where:{email}
-        })
-        return user
-    },
-    createUser: async (user) => {
-        return await prisma.user.create({data:user})
-    },
-    getUserById: async (id) => {
-        const user = await prisma.user.findUnique({
-            where:{
-                id
-            }
-        })
-        return user
-    }
-}
+export const repositoryMethods: IRepositoryContract = {
+    getUserByEmail: async (email) => await prisma.user.findUnique({ where: { email } }),
+    createUser: async (user) => await prisma.user.create({ data: user }),
+    getUserById: async (id) => await prisma.user.findUnique({ 
+        where: { id }, 
+        include: { addresses: true } 
+    }),
+    
+    createAddress: async (userId, city, street) => 
+        await prisma.address.create({ data: { userId, city, street } }),
+        
+    getAddresses: async (userId) => 
+        await prisma.address.findMany({ where: { userId } }),
+    getAddressById: async (id) => 
+        await prisma.address.findUnique({ where: { id } }),
+        
+    updateAddress: async (id, city, street) => 
+        await prisma.address.update({ where: { id }, data: { city, street } }),
+        
+    deleteAddress: async (id) => 
+        await prisma.address.delete({ where: { id } }),
+        
+    updateUser: async (id, data) => 
+        await prisma.user.update({ where: { id }, data }),       
+    deleteUser: async (id) => 
+        await prisma.user.delete({ where: { id } })
+};
